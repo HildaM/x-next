9cebba53a61fa78f3d7c8898a9ba8057604c5b2a
HildaM <hilda_quan@163.com>
2024-05-22 18:03:13 +0800

    Add next-auth and modify sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,13 @@
+"use client"
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { SiOpenaigym } from "react-icons/si";
 import { TiHome } from "react-icons/ti";
 
 export default function Sidebar() {
+    const { data: session } = useSession()
+
     return (
         <div className="flex flex-col gap-4 p-3">
             <Link href="/">
@@ -14,10 +19,23 @@ export default function Sidebar() {
                 <span className="font-bold hidden xl:inline">Home</span>
             </Link>
 
-            <button className="bg-blue-400 text-white rounded-full mt-4 hover:brightness-95
-                transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline">
-                Sign In
-            </button>
+            {/* 根据session来判断用户是否登录 */}
+            {session ? (
+                <button className="bg-blue-400 text-white rounded-full mt-4 hover:brightness-95
+                    transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+                    onClick={() => signOut()}
+                >
+                    Sign Out
+                </button>
+            ) : (
+                <button className="bg-blue-400 text-white rounded-full mt-4 hover:brightness-95
+                    transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline"
+                    onClick={() => signIn()}
+                >
+                    Sign In
+                </button>
+            )}
+
         </div>
     )
 }
